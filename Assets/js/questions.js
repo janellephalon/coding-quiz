e5719a9361f1291b201c80ed63b971a5ad54c7e8
Janelle <janellephalon@Janelles-Air.home>
2022-01-15 11:54:19 -0500

    Updated organization, and JS

---
+++ Assets/js/questions.js
@@ -0,0 +1,32 @@
+// questions 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    }, 
+
+    {
+        title: "The condition in an if/else statement is enclosed with _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"], 
+        answer: "parentheses"
+    },
+
+    {
+        title: "What JavaScript method can we use to select an HTML element?",
+        choices: ["document.querySelector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    }, 
+
+    {
+        title: "What HTML tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+
+    {
+        title: "What attribute is used in HTML to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+]
